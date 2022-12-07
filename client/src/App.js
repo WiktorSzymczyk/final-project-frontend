39f307029ab521bde115d4ce76aee3dcdae600d4
koziu33en <wiktor.szymczyk221@gmail.com>
2022-12-07 20:48:34 +0000

    Added basic monorepo and added Navbar and Chat component

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Chat from "./components/Chat/Chat";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,32 @@
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+const socket = io.connect("http://localhost:3001");
+
+function Chat() {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+  const sendMessage = () => {
+    socket.emit("send_message", { message });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+
+  return (
+    <div className="App">
+      <input
+        placeholder="Message..."
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }}
+      />
+      <button onClick={sendMessage}>Send Message</button>
+      <h1>Message: {messageReceived}</h1>
+    </div>
+  );
+}
+
+export default Chat;
